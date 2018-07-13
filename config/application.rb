a1ce15026d99525276c43e94d2ce53a310dc118c
JientDoNgoc <dongocthuyoanh.07.05@gmail.com>
2018-07-13 08:40:53 +0000

    Implemented initial connection to Twitter API

--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,7 @@ module JientPorfolio
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.generators.javascript_engine = :js
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Don't generate system test files.
+    config.eager_load_paths << "#{Rails.root}/lib"
     config.generators.system_tests = nil
   end
 end
